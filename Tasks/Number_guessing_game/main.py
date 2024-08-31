90bbc926a913d16f626d591647048ad762f6174a
olbrimek <vladimir.olbrimek@seznam.cz>
2024-08-31 11:22:33 +0200

    end

--- Tasks/Number_guessing_game/main.py
+++ Tasks/Number_guessing_game/main.py
@@ -2,10 +2,23 @@ import random
 
 def get_number():
  try:
-    result = int(input("Guess the number: "))
+    result = int(input("Write the number: "))
     return result
  except ValueError:
      print("That's not an integer.")
+
+def guess_the_number():
+    """Main function"""
+    secret_number = random.randint(1, 100)
+    given_number = get_number()
+    while given_number != secret_number:
+        if given_number < secret_number:
+            print("Too small!")
+        else:
+            print("Too big!")
+        given_number = get_number()
+    print("You Win!")
+guess_the_number()
 
 
 
